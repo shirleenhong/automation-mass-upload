defd34e6892a6650634f8e99f8bf2046ab095f06
shirleenhong <47838535+shirleenhong@users.noreply.github.com>
2024-11-29 09:26:20 +0800

    Add files via upload

---
+++ MassUpload/src/main/java/ServiceNow/MassUpload/App.java
@@ -0,0 +1,13 @@
+package ServiceNow.MassUpload;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
